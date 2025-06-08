2c06fe67c111307a4edddaf21f3060a7b42071c3
Chuang Li <657576638@qq.com>
2025-06-08 13:09:47 +0800

    feat：some binary tree

--- Leetecode/tree.go
+++ Leetecode/tree.go
@@ -1,5 +1,9 @@
 package Leetecode
 
+import (
+	"math"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -82,4 +86,132 @@ func maxDepth(root *TreeNode) int {
 	leftMaxDepth := maxDepth(root.Left)
 	rightMaxDepth := maxDepth(root.Right)
 	return 1 + max(leftMaxDepth, rightMaxDepth)
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	queue := make([]*TreeNode, 0)
+	offer := func(node *TreeNode) {
+		if node != nil {
+			queue = append(queue, node)
+		}
+	}
+	poll := func() *TreeNode {
+		node := queue[0]
+		queue = queue[1:]
+		return node
+	}
+
+	offer(root)
+	ans := make([][]int, 0)
+	for len(queue) > 0 {
+		count := len(queue)
+		subAns := make([]int, 0)
+		for i := 0; i < count; i++ {
+			node := poll()
+			subAns = append(subAns, node.Val)
+			offer(node.Left)
+			offer(node.Right)
+		}
+		ans = append(ans, subAns)
+	}
+	return ans
+}
+
+/*
+*
+[-10,-3,0,5,9]
+*/
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	if len(nums) == 1 {
+		return &TreeNode{Val: nums[0]}
+	}
+
+	mid := len(nums) / 2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBST(nums[:mid])
+	if mid+1 > len(nums)-1 {
+		root.Right = nil
+	} else {
+		root.Right = sortedArrayToBST(nums[mid+1 : len(nums)-1])
+	}
+	return root
+}
+
+var list []*TreeNode
+
+func isValidBSTCore(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	leftIs := isValidBSTCore(root.Left)
+	if !leftIs {
+		return false
+	}
+	if len(list) == 0 {
+		list = append(list, root)
+	} else {
+		before := list[len(list)-1]
+		if before.Val >= root.Val {
+			return false
+		} else {
+			list = append(list, root)
+		}
+	}
+
+	rightIs := isValidBSTCore(root.Right)
+	if !rightIs {
+		return false
+	}
+	return true
+}
+
+var maxPathSumAns = math.MinInt32
+
+func maxPathSum(root *TreeNode) int {
+	maxPathSumAns = 0
+	maxPathSumRecur(root)
+	return maxPathSumAns
+}
+
+func maxPathSumRecur(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftMax := maxPathSumRecur(root.Left)
+	rightMax := maxPathSumRecur(root.Right)
+
+	maxPathSumAns = max(maxPathSumAns, leftMax+rightMax+root.Val)
+	return max(0, root.Val+max(leftMax, rightMax))
+}
+
+func rightSideView(root *TreeNode) []int {
+	queue := make([]*TreeNode, 0)
+	offer := func(node *TreeNode) {
+		if node != nil {
+			queue = append(queue, node)
+		}
+	}
+	poll := func() *TreeNode {
+		node := queue[0]
+		queue = queue[1:]
+		return node
+	}
+
+	offer(root)
+	result := make([]int, 0)
+	for len(queue) > 0 {
+		count := len(queue)
+		for i := 0; i < count; i++ {
+			node := poll()
+			if i == count-1 {
+				result = append(result, node.Val)
+			}
+			offer(node.Left)
+			offer(node.Right)
+		}
+	}
+	return result
 }
